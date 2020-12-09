257eeafd9d3aeae587e0f68ec2eab9cda5ade2ba
David Oster <paspa@hotmail.com>
2020-12-09 12:16:34 +0200

    updated doGet
    - doGet forwards to the view newproduct.jsp

--- src/java/controllers/Product.java
+++ src/java/controllers/Product.java
@@ -6,7 +6,9 @@
 package controllers;
 
 import java.io.IOException;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,13 +18,15 @@ import services.ProductService;
  *
  * @author George.Pasparakis
  */
+@WebServlet(name = "product", urlPatterns = {"/product"})
 public class Product extends HttpServlet {
     ProductService ps;
     
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) 
             throws ServletException, IOException {
-        
+        RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/views/newproduct.jsp");
+        rd.forward(req, resp);
     }
 
     @Override
